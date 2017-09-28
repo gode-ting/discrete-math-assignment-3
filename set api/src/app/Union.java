ebb189ae0e96bac21f4045b66e333a202d0a8ac5
Daniel Hillmann <hiller@live.dk>
2017-09-29 00:02:18 +0200

    updates. Add union method

--- set api/src/app/Union.java
+++ set api/src/app/Union.java
@@ -1,25 +1,52 @@
 package app;
 
 import app.Interfecaes.UnionInterface;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-public class Union implements UnionInterface{
+public class Union implements UnionInterface {
+
+    public List<Integer> unionArrays(int[] a, int[] b) {
+        int aSize = a.length;
+        int bSize = b.length;
+
+        HashMap<Integer, Integer> myMap = new HashMap<Integer, Integer>();
+
+        for (int i = 0; i < aSize; i++) {
+            myMap.put(a[i], a[i]);
+        }
+        
+        for (int i = 0; i < bSize; i++) {
+            myMap.put(b[i], b[i]);
+        }
+
+        ArrayList unionSet = new ArrayList();
+
+        for(Map.Entry<Integer, Integer> value: myMap.entrySet()) {
+
+            if (myMap.containsKey(value.getValue())) {
+                unionSet.add(value.getValue());
+            }
+        }
+        return unionSet;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1, 3, 4, 5, 7, 8};
+        int[] b = {2, 3, 4, 6, 7, 9, 13};
+
+        Union union = new Union();
+        System.out.println("Union array: " + union.unionArrays(a, b));
+    }
 
     @Override
     public int[] noDuplicates(int[] a, int[] b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
-        int aSize = a.getSize();
-        int bSize = b.getSize();
- 
-        if (aSize == 0 && bSize == 0) {
-        } else {
-            
-        }
-        
-        return;
-    }
-    
-    public static void main(String[] args) {
-        
-    }
-    
 }
