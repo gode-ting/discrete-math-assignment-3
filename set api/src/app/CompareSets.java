0768dc0507477fec6343658c9c3a7b05dec9b8e4
Anders Bjergfelt <abj@dis-play.dk>
2017-09-29 21:23:44 +0200

    mod classes

--- set api/src/app/CompareSets.java
+++ set api/src/app/CompareSets.java
@@ -7,13 +7,37 @@ package app;
 
 import app.Interfecaes.CompareSetInterface;
 
-public class CompareSets implements CompareSetInterface {
+public class CompareSets<T extends Comparable> implements CompareSetInterface {
 
-    public int compareSets(int[] a, int[] b) {
-        if (a.length == 0) {
-            return b.length == 0 ? 0 : -1;
+    public static void main(String[] args) {
+        Integer[] listA = {3,4,5,7,4,3,2,1,10};
+        Integer[] listB = {3,4,5,7,4,3,2,1,10};
+        
+        String[]  listC = {"a","b","c"};
+        Integer[] listD = {3,4,5,7,4,3,2,1,12,11};
+        Set<Integer> o1 = new Set<>(listA);
+        Set<Integer> o2 = new Set<>(listB);
+        Set<Integer> o3 = new Set<>(listD);
+        
+        
+        Set<String> o4 = new Set<>(listC);
+       
+        CompareSets cs = new CompareSets();
+        
+        System.out.println(cs.compareSets(o1,o2));
+    }
+    
+    public  int compareSets(Set<T> a, Set<T> b) {
+        
+        if (a.getValues().getClass() != b.getValues().getClass()) {
+            return 2;
+        }else{
+          
         }
-        if (b.length == 0) {
+        if (a.getValues().length == 0) {
+            return b.getValues().length == 0 ? 0 : -1;
+        }
+        if (b.getValues().length == 0) {
             return 1;
         }
 
@@ -21,17 +45,21 @@ public class CompareSets implements CompareSetInterface {
         int indexA = 0;
         int indexB = 0;
 
-        while (indexA < a.length && indexB < b.length) {
-            if (a[indexA] == b[indexB]) {
+        while (indexA < a.getValues().length && indexB < b.getValues().length) {
+           
+            if (a.getValues()[indexA] == b.getValues()[indexB]) {
+             
                 indexA++;
                 indexB++;
-            } else if (a[indexA] > b[indexB]) {
+                
+                //System.out.println("IndexA: " + indexA + "IndexB: " + indexB);
+            } else if (a.getValues()[indexA].compareTo(b.getValues()[indexB]) == -1) {
                 if (result == 1) {
                     return -2;
                 }
                 indexB++;
                 return -1; // a is subset
-            } else if (a[indexA] < b[indexB]) {
+            } else if (a.getValues()[indexA].compareTo(b.getValues()[indexB])== 1) {
                 if (result == -1) {
                     return -2;
                 }
@@ -39,10 +67,15 @@ public class CompareSets implements CompareSetInterface {
                 result = 1; // a is superset
             }
         }
-        if (indexA == a.length && indexB == b.length) {
+        if (indexA == a.getValues().length && indexB == b.getValues().length) {
             return result;
         }
         return -2; // sets
+    }
+
+    @Override
+    public int compareSets(int[] a, int[] b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
